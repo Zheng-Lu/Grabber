00a103821ec846f19c3245fefcc5c70b74967660
Zheng-Lu <s2085615@ed.ac.uk>
2021-07-15 09:28:42 +0800

    解决了os.listdir中文件乱序的问题

--- PicCrawler.py
+++ PicCrawler.py
@@ -4,6 +4,7 @@ import pandas as pd
 import urllib
 import urllib.request
 import csv
+import re
 import os
 import base64
 import hashlib
@@ -20,7 +21,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-
 path = 'D://picSearch//'
 url = 'https://adm.mixshop.world/distributor/product/list?pageNum=1&pageSize=100'
 headers = {
@@ -35,22 +35,22 @@ pics = []
 for i in range(len(content['data']['list'])):
     pics.append(content['data']['list'][i]['pic'])
 
-urls = pd.read_csv('C://Users//Lenovo//Desktop//mixshop_pic.csv') #save the url list as a dataframe
+urls = pd.read_csv('C://Users//Lenovo//Desktop//mixshop_pic.csv')  # save the url list as a dataframe
 rows = []
 for index, i in urls.iterrows():
     rows.append(i[-1])
 
 counter = 0
 
-#print(rows)
-#print(pics)
+# print(rows)
+# print(pics)
 
 for i in pics:
     file_name = 'product' + str(counter) + '.jpg'
     path_name = path + file_name
     print(file_name)
     response = requests.get(i)
-    file = open(file_name, "wb")
+    file = open(path_name, "wb")
     file.write(response.content)
     file.close()
     counter += 1
@@ -61,6 +61,7 @@ for i in pics:
 EXTEND = [".bmp", ".jpg", ".jpeg", ".tif", ".tiff",
           ".jfif", ".png", ".gif", ".iff", ".ilbm"]
 
+
 def is_img(img_path):
     # 根据后缀判断是否为图片
     ext = os.path.splitext(img_path)[1]
@@ -69,8 +70,13 @@ def is_img(img_path):
     else:
         return False
 
+def sorted_alphanumeric(data):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
+    return sorted(data, key=alphanum_key)
+
 def getfilelist(path, filelist):
-    file = os.listdir(path)
+    file = sorted_alphanumeric(os.listdir(path))
     for img_name in file:
         if os.path.isdir(os.path.join(path, img_name)):
             getfilelist(os.path.join(path, img_name), filelist)
@@ -79,23 +85,25 @@ def getfilelist(path, filelist):
                 name = os.path.join(path, img_name)
                 filelist.append(name)
 
+
 def partition(ls, size):
-    num_per_list = len(ls)//size
+    num_per_list = len(ls) // size
     result = []
-    if num_per_list*size == len(ls):
+    if num_per_list * size == len(ls):
         for i in range(size):
-            result.append(ls[num_per_list*i:num_per_list*(i+1)])
+            result.append(ls[num_per_list * i:num_per_list * (i + 1)])
     else:
-        for i in range(size-1):
-            result.append(ls[num_per_list*i:num_per_list*(i+1)])
-        result.append(ls[num_per_list*(size-1):])
+        for i in range(size - 1):
+            result.append(ls[num_per_list * i:num_per_list * (i + 1)])
+        result.append(ls[num_per_list * (size - 1):])
     return result
 
 USERNAME = os.environ['USERNAME']
+
 class GoogleSearcher:
     def __init__(self, download="download", sleep_time=1):
         super().__init__()
-        self._download = download # 下载文件夹
+        self._download = download  # 下载文件夹
         self.sleep_time = sleep_time  # 下载页面时等待时间
         self.header = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"}
@@ -106,10 +114,10 @@ class GoogleSearcher:
         # self.option.add_argument("--user-data-dir=" + f"C:/Users/{USERNAME}/AppData/Local/Google/Chrome/User Data/")
         # self.option.add_argument("headless")  # if use headless, may failed.
         self.option.add_argument("disable-gpu")
-        self.driver = webdriver.Chrome(options=self.option) # 以上为浏览器对象创建
+        self.driver = webdriver.Chrome(options=self.option)  # 以上为浏览器对象创建
 
     def upload_img_get_html(self, file):
-    	# 上传图片并转到图片列表页面
+        # 上传图片并转到图片列表页面
         print(
             f"{Fore.GREEN} Begin to upload image {os.path.split(file)[1]} {Fore.RESET}")
         self.driver.get("https://www.google.com/imghp")
@@ -177,7 +185,7 @@ class GoogleSearcher:
         return elem
 
     def get_extension_from_link(self, link, default='jpg'):
-    # 获取文件后缀
+        # 获取文件后缀
         splits = str(link).split('.')
         if len(splits) == 0:
             return default
@@ -192,13 +200,13 @@ class GoogleSearcher:
             return default
 
     def base64_to_object(self, src):
-    # base64 解码
+        # base64 解码
         header, encoded = str(src).split(',', 1)
         data = base64.decodebytes(bytes(encoded, encoding='utf-8'))
         return data
 
     def download_images(self, links, download_dir):
-    # 下载图片
+        # 下载图片
         total = len(links)
         for index, link in enumerate(links):
             try:
@@ -207,27 +215,27 @@ class GoogleSearcher:
                 else:
                     print(
                         'Downloading {} : {} / {}'.format(link[:100], index + 1, total))
-                        # 链接过长，只打印部分
+                    # 链接过长，只打印部分
                 if str(link).startswith('data:image/jpeg;base64'):
-                # base64编码的jpg图片
+                    # base64编码的jpg图片
                     response = self.base64_to_object(src=link)
                     ext = 'jpg'
                     is_base64 = True
                 elif str(link).startswith('data:image/png;base64'):
-                # base64编码的png图片
+                    # base64编码的png图片
                     response = self.base64_to_object(src=link)
                     ext = 'png'
                     is_base64 = True
                 else:
-                # 图片超链接
+                    # 图片超链接
                     response = requests.get(link, stream=True, timeout=5)
                     ext = self.get_extension_from_link(link=link)
                     is_base64 = False
 
-                path = os.path.join(download_dir, str(index).zfill(4)+"."+ext)
+                path = os.path.join(download_dir, str(index).zfill(4) + "." + ext)
                 try:
                     with open(path, "wb") as f:
-                    # base64图片和超链接图片两种保存方法
+                        # base64图片和超链接图片两种保存方法
                         if is_base64:
                             f.write(response)
                         else:
@@ -270,32 +278,32 @@ class GoogleSearcher:
                     if len(src) < 100:
                         print('%d: %s' % (count, src))
                     else:
-                        print('%d: %s' % (count, src[:100])) # 如果太长，只打印一部分
+                        print('%d: %s' % (count, src[:100]))  # 如果太长，只打印一部分
                     count += 1
             except StaleElementReferenceException:
                 pass
             except Exception as e:
                 print(
                     '[Exception occurred while collecting links from google_full] {}'.format(e))
-            scroll = self.driver.execute_script("return window.pageYOffset;") # 页面滚动的位置
+            scroll = self.driver.execute_script("return window.pageYOffset;")  # 页面滚动的位置
             if scroll == last_scroll:
-            # 页面滚动1
+                # 页面滚动1
                 scroll_patience += 1
             else:
                 scroll_patience = 0
                 last_scroll = scroll
             if scroll_patience >= 30:
-            #页面滚动30，停止
+                # 页面滚动30，停止
                 break
             elem.send_keys(Keys.RIGHT)
-        links = list(dict.fromkeys(links)) # 链接去重
+        links = list(dict.fromkeys(links))  # 链接去重
         print('Collect links done. Total: {}'.format(len(links)))
         return links
 
     def simple_file_run(self, img):
         # 上传图片并进行搜索
-        img_name = os.path.splitext(os.path.split(img)[1])[0] # 图片名
-        parent_name = os.path.split(os.path.split(img)[0])[-1] # 图片的父级名字，用来区分图片的类别
+        img_name = os.path.splitext(os.path.split(img)[1])[0]  # 图片名
+        parent_name = os.path.split(os.path.split(img)[0])[-1]  # 图片的父级名字，用来区分图片的类别
         print("--> Processing image:  {}  ".format(img_name))
         download_dir = os.path.join(self._download, parent_name, img_name)
         os.makedirs(download_dir, exist_ok=True)
@@ -308,20 +316,21 @@ class GoogleSearcher:
         print("{}Image {} finished\n{}".format(
             Fore.GREEN, img_name, Fore.RESET))
 
+
 def download_task(filelist):
     searcher = GoogleSearcher(
         download=r"./download")
     for file in filelist:
         searcher.simple_file_run(file)  # 上传单张图并进行以图搜图
 
-def run():
-    num_process = cpu_count() # 进程数设置为cpu核心数
-    pool = Pool(num_process) # 建立一个进程池
-    filelist = []
-    upload = r"./upload" # 需要进行上传的图片文件夹
-    getfilelist(upload, filelist)  # 递归查找文件夹里面所有的图片文件
-    result = partition(filelist, num_process) # 将图片文件列表平均分为几个list，每个进程跑一部分
-    pool.map_async(download_task, result) # 下载任务丢进进程池
-    pool.close() # 不再允许加入进程池
-    pool.join() # 等待进程完成
 
+def run():
+    num_process = cpu_count()  # 进程数设置为cpu核心数
+    pool = Pool(num_process)  # 建立一个进程池
+    filelist = []
+    upload = r"./upload"  # 需要进行上传的图片文件夹
+    getfilelist(upload, filelist)  # 递归查找文件夹里面所有的图片文件
+    result = partition(filelist, num_process)  # 将图片文件列表平均分为几个list，每个进程跑一部分
+    pool.map_async(download_task, result)  # 下载任务丢进进程池
+    pool.close()  # 不再允许加入进程池
+    pool.join()  # 等待进程完成
