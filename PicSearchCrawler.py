7b965cdc57c7a1b678079b98c6c5bc431de5d040
Zheng-Lu <s2085615@ed.ac.uk>
2021-07-27 15:24:22 +0800

    Crawler that using image as a key word to scrape corresponding information from Google

--- PicSearchCrawler.py
+++ PicSearchCrawler.py
@@ -1,5 +1,6 @@
 import os
 from multiprocessing import Pool, cpu_count
+from selenium import webdriver
 
 path = r"C:\Users\Lenovo\Desktop\pics"
 
@@ -7,6 +8,7 @@ path = r"C:\Users\Lenovo\Desktop\pics"
 def run():
     num_process = cpu_count()
     pool = Pool(num_process)
+    result = partition(imgList(path),num_process)
 
 
 def imgList(path):
@@ -16,5 +18,22 @@ def imgList(path):
         file_list.append(img)
     return file_list
 
-# def partition():
+def partition(ls, size):
+    num_per_list = len(ls) // size
+    result = []
+    if num_per_list * size == len(ls):
+        for i in range(size):
+            result.append(ls[num_per_list * i:num_per_list * (i + 1)])
+    else:
+        for i in range(size - 1):
+            result.append(ls[num_per_list * i:num_per_list * (i + 1)])
+        result.append(ls[num_per_list * (size - 1):])
+    return result
+
+class GooglePicSearcher:
+    def __init__(self):
+        self.option = webdriver.ChromeOptions()
+        self.driver = webdriver.Chrome(options=self.option)
+
+
 
