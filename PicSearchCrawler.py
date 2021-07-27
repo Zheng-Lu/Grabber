c507c9a654dcff6dd36ba730cd3f12b9957f6169
Zheng-Lu <s2085615@ed.ac.uk>
2021-07-27 16:07:10 +0800

    Upload image function added

--- PicSearchCrawler.py
+++ PicSearchCrawler.py
@@ -1,6 +1,11 @@
 import os
 from multiprocessing import Pool, cpu_count
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 path = r"C:\Users\Lenovo\Desktop\pics"
 
@@ -8,7 +13,7 @@ path = r"C:\Users\Lenovo\Desktop\pics"
 def run():
     num_process = cpu_count()
     pool = Pool(num_process)
-    result = partition(imgList(path),num_process)
+    result = partition(imgList(path), num_process)
 
 
 def imgList(path):
@@ -17,6 +22,7 @@ def imgList(path):
     for img in file:
         file_list.append(img)
     return file_list
+
 
 def partition(ls, size):
     num_per_list = len(ls) // size
@@ -30,10 +36,34 @@ def partition(ls, size):
         result.append(ls[num_per_list * (size - 1):])
     return result
 
+
 class GooglePicSearcher:
     def __init__(self):
+        self.header = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
         self.option = webdriver.ChromeOptions()
         self.driver = webdriver.Chrome(options=self.option)
 
+    def upload_img(self):
+        self.driver.get("https://images.google.com")
 
+        # 等待相机按钮出现
+        condition_1 = EC.visibility_of_element_located(
+            (By.CLASS_NAME, "LM8x9c"))
+        WebDriverWait(self.driver, timeout=20,
+                      poll_frequency=0.5).until(condition_1)
+        # 相机按钮出现后点击
+        image_button = self.driver.find_element_by_class_name("LM8x9c")
+        image_button.send_keys(Keys.ENTER)
+
+        # 等待出现上传图片字样
+        condition_2 = EC.visibility_of_element_located(
+            (By.ID, "dRSWfb"))
+        WebDriverWait(self.driver, timeout=20, poll_frequency=0.5).until(
+            condition_2)
+
+        # 点击上传图片
+        upload = self.driver.find_element_by_xpath('//*[@id="dRSWfb"]/div/a')
+        upload.send_keys(Keys.ENTER)
 
